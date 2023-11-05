f0b8bf5f756f9e41edc5f71ff0fe279b4f564bb9
AdrianTsybukh <adruantsybukh@gmail.com>
2023-11-05 16:54:15 +0200

    Changing the algorithm for displaying elements with machines

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -1,25 +1,35 @@
 import styles from './Home.module.css'
+import { cars } from './cars.data.ts'
 
 function App() {
   return (
     <div>
       <h1>Cars catalog</h1>
+
       <div>
         <div>
-          <div className={styles.item}>
-            <div
-              className={styles.image}
-              style={{
-                backgroundImage: 'url(/bmw-x7.jpg)',
-              }}
-            />
+          {cars.map((car) => (
+            <div key={car.id} className={styles.item}>
+              <div
+                className={styles.image}
+                style={{
+                  backgroundImage: `url(${car.image})`,
+                }}
+              />
 
-            <div className={styles.info}>
-              <h2>Car 1</h2>
-              <p>100 000$</p>
-              <button>Read more</button>
+              <div className={styles.info}>
+                <h2>{car.name}</h2>
+                <p>
+                  {new Intl.NumberFormat('en-US', {
+                    style: 'currency',
+                    currency: 'USD',
+                    currencyDisplay: 'narrowSymbol',
+                  }).format(car.price)}
+                </p>
+                <button>Read more</button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>

c2d23129a246fecddd2ccc23eebc90e3dc82fc10
AdrianTsybukh <adruantsybukh@gmail.com>
2023-11-05 16:52:50 +0200

    Adding images and simple database

---
+++ src/components/screens/home/cars.data.ts
@@ -0,0 +1,14 @@
+export const cars = [
+  {
+    id: 1,
+    name: 'Toyota Camry',
+    price: 24970,
+    image: 'toyota-camry.jpg',
+  },
+  {
+    id: 2,
+    name: 'Honda Civic',
+    price: 22545,
+    image: 'honda-civic.webp',
+  },
+]
